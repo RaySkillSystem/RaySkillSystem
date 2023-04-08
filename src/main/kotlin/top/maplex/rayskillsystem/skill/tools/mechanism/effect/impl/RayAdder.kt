7b5be94614a788d6e68c8dd72109d07664a4cc34
FxRayHughes <bushuaichen@yeah.net>
2023-04-08 11:03:13 +0800

    新增召唤物

--- src/main/kotlin/top/maplex/rayskillsystem/skill/tools/mechanism/effect/impl/RayAdder.kt
+++ src/main/kotlin/top/maplex/rayskillsystem/skill/tools/mechanism/effect/impl/RayAdder.kt
@@ -1,20 +1,42 @@
 package top.maplex.rayskillsystem.skill.tools.mechanism.effect.impl
 
+import org.bukkit.Material
 import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
+import taboolib.common.platform.function.submit
 import taboolib.common.util.Location
 import taboolib.module.effect.shape.Ray
 import taboolib.platform.util.toBukkitLocation
 import java.util.*
 
-fun Ray.backShow(action: Location.() -> Unit = {}, near: LivingEntity.() -> Unit = {}): Ray {
+
+fun Ray.backShow(
+    //是否可以穿墙 true 则不可以
+    wall: Boolean = true,
+    //动画播放速度
+    period: Long = 0L,
+    //播放时执行
+    action: Location.() -> Unit = {},
+    //碰到实体后执行
+    near: LivingEntity.() -> Unit = {},
+    //结束时执行
+    over: () -> Unit = {},
+): Ray {
     var i = 0.0
-    while (i < maxLength) {
+    submit(period = period) {
+        if (i > maxLength) {
+            over.invoke()
+            cancel()
+        }
         val vectorTemp = direction.clone().multiply(i)
         val spawnLocation = origin.clone().add(vectorTemp)
         spawnParticle(spawnLocation)
         action.invoke(spawnLocation)
+        //每一帧只会给一个目标造成一次伤害
         val list = mutableListOf<UUID>()
+        if (wall && spawnLocation.toBukkitLocation().block.type != Material.AIR) {
+            over.invoke()
+            return@submit
+        }
         spawnLocation.toBukkitLocation().world?.getNearbyEntities(spawnLocation.toBukkitLocation(), 0.2, 0.2, 0.2) {
             it is LivingEntity
         }?.forEach {
@@ -27,6 +49,8 @@ fun Ray.backShow(action: Location.() -> Unit = {}, near: LivingEntity.() -> Unit
         }
         i += step
     }
+
+
     return this
 }
 
